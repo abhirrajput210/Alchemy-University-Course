2e2a09fa156c8ec07aefb9a686301c7e263f3d12
abhirrajput210 <abhishekrajput.lamprostech@gmail.com>
2023-06-21 12:58:14 +0530

    Completed Week 3 Task

--- Week 3/Weekly Task/src/App.js
+++ Week 3/Weekly Task/src/App.js
@@ -17,6 +17,15 @@ function App() {
   const [transactions, setTransactions] = useState([]);
   const [showTransactions, setShowTransactions] = useState(false);
   const [selectedTransaction, setSelectedTransaction] = useState(null);
+  const [address, setAddress] = useState('');
+  const [balance,setBalance] = useState(0);
+  const [network, setNetwork] = useState("");
+
+  function handleNetworkChange(event) {
+    const { name, value } = event.target;
+    console.log(value)
+    setNetwork(value);
+  }
 
   useEffect(() => {
     async function getBlockData() {
@@ -34,7 +43,8 @@ function App() {
 
       const getBlockWithTransactions = await alchemy.core.getBlockWithTransactions(getBlockHash);
       console.log(getBlockWithTransactions.transactions);
-      setTransactions(getBlockWithTransactions.transactions.slice(0, 10));
+      setTransactions(getBlockWithTransactions.transactions);
+      console.log(getBlockWithTransactions.transactions);
     }
 
     getBlockData();
@@ -43,6 +53,21 @@ function App() {
   const handleGetBlockTransactions = () => {
     setShowTransactions(true);
   };
+
+  const displayBalance = async () => {
+
+    const settingsBalance = {
+      apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+      network: network,
+    };
+    
+    const alchemyBalance = new Alchemy(settingsBalance);
+
+    let response = await alchemyBalance.core.getBalance(address, "latest")
+    let balanceHex = response._hex;
+    setBalance(parseInt(balanceHex, 16) / Math.pow(10, 18));
+    // console.log("Response",response);
+  }
 
   const handleButtonClick = async (hash) => {
     try {
@@ -72,7 +97,10 @@ function App() {
 
         {showTransactions && (
           <div style={{marginTop:"100px", marginBottom:"100px"}}>
-          <table >
+            <h2>Block Transactions</h2>
+            <div style={{alignItems:'center',justifyContent:'center',display:'flex'}}>
+            <div className='transaction_table'>
+          <table>
             <thead>
               <tr>
                 <th>Transaction Hash</th>
@@ -90,6 +118,8 @@ function App() {
               ))}
             </tbody>
           </table>
+          </div>
+          </div>
           </div>
         )}
 
@@ -122,6 +152,48 @@ function App() {
             </table>
           </div>
         )}
+        <div style={{marginTop:"100px",alignItems:"center",justifyContent:"center",marginBottom:"100px"}}>
+        <h2>Account Balance</h2>
+        <div style={{marginTop:"10px",marginBottom:"10px"}}>  
+        <select
+                    name="networkList"
+                    id="networkList"
+                    onChange={handleNetworkChange}
+                    defaultValue="Choose your network"
+                    style={{width:"616px", height:"50px",fontSize:'1.2rem'}}
+                >
+                    <option defaultValue="Choose your network" />
+                    <option value="Choose your network">Choose your network</option>
+                    <option value="eth-mainnet">Ethereum Mainnet</option>
+                    <option value="eth-goerli">Ethereum Goerli</option>
+                    <option value="polygon-mumbai">Polygon Mumbai</option>
+                </select>
+                </div>
+        <input 
+          style={{width:"600px", height:"50px",fontSize:'1.2rem',paddingLeft:"10px"}}
+          type='text' 
+          placeholder='Enter Your Account Address'
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}></input>
+        <div style={{marginTop:"10px",marginBottom:"10px"}}>
+
+        <input 
+          style={{width:"600px", height:"50px",fontSize:'1.2rem',paddingLeft:"10px"}}
+          type='text' 
+          value={balance + " ETH"}
+          onChange={(e) => setAddress(e.target.value)}
+          readOnly></input>
+        </div>
+
+          <div style={{marginTop:"10px",marginBottom:"10px"}}>
+          <button 
+            className='account_balance_btn'
+            onClick={displayBalance}
+            style={{ marginTop: '10px', fontSize:'1rem', marginLeft:"10px"}}>
+            CHECK ACCOUNT BALANCE 
+          </button>
+          </div>
+      </div>
       </div>
     </>
   );
